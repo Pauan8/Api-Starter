6e11bfef774d797dd53ac6e925e51275acdca4f1
Pauan8 <pauline.j.andersson@vgregion.se>
2021-05-03 01:12:05 +0200

    .

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import data from './data.json'
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -17,6 +18,25 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
+
+app.get('/API', (req, res) => {
+ res.json(data)
+})
+
+app.get('/Category/:Category', (req, res) => {
+  const Category = req.params.Category;
+  const hasKey = req.query.hasKey;
+  const hasHttps = req.query.hasHttps;
+  let APIbyCategory = data.entries.filter(item => item.Category === Category)
+  if(hasKey){
+    APIbyCategory = APIbyCategory.filter(item => item.Auth === "apiKey")
+  } 
+  if(hasHttps){
+    APIbyCategory = APIbyCategory.filter(item => item.HTTPS === true)
+  }
+  res.json(APIbyCategory)
+ })
+
 
 // Start the server
 app.listen(port, () => {
